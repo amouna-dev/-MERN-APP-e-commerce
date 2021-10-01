5bf133b192840eb6e331c95ddc490764e4432c7f
amouna-dev <amouna_1387@yahoo.fr>
2021-10-01 18:45:01 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const connectDB = require('./config/connectDB')
+require('dotenv').config('./config/.env')
+const cors = require('cors')
+
+const authRouter = require('./Routes/Auth')
+const userRouter = require('./Routes/user')
+const productRouter = require('./Routes/product')
+const orderRouter = require('./Routes/order')
+
+
+
+const app = express()
+const port = process.env.PORT || 6000 
+
+connectDB()
+
+app.use(cors())
+
+//middleware
+app.use(express.json())
+
+
+//use routes
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/order', orderRouter)
+
+
+app.get('/api/config/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
+})
+app.listen(port, (err) => {
+    err?
+    console.error(err)
+    :
+    console.log(`Server is running on port ${port}`)
+})
